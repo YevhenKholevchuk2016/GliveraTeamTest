3c962a9d19ee35fad73a1ff39e59f59ea3b5e162
Yevhen Kholevchuk <holevchuk.evgeny+main@gmail.com>
2024-10-07 23:29:56 +0300

    fix: fix path, README update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GliveraTeamTest
 Glivera-team test task
+
+### Frontend build
+Change directory to **source** and run one of the following command:
+
+1. `npm run start` - install **node_modules**, build static for production
+2. `npm run dev` - rebuild **node-sass**, build static for development with Browsersync
+3. `npm run build` - install **node_modules**, rebuild **node-sass**, build static for production
+
+Aliases for HTML images are in `source/gulp-tasks/html.js` file.
--- source/gulp-tasks/html.js
+++ source/gulp-tasks/html.js
@@ -18,9 +18,9 @@ export default function html(cb, inputHtml, basepath, outputHtml) {
         prefix: '@@',
         basepath,
         context: {
-          images_common_path__: '/assets/images/pages/common/',
-          fonts_poppins__: '/assets/fonts/poppins/',
-          sprites_common__: '/assets/images/sprites/common/svg/symbols.svg#icon-',
+          images_common_path__: 'assets/images/pages/common/',
+          fonts_poppins__: 'assets/fonts/poppins/',
+          sprites_common__: 'assets/images/sprites/common/svg/symbols.svg#icon-',
         },
       }),
       dest(outputHtml),
